e579043b581d91c915a1fa64259052a37d2882ed
Shai_Base <shaibase1328@gmail.com>
2024-01-29 13:04:47 +0800

    added function

--- Exercise-15.py
+++ Exercise-15.py
@@ -1,6 +1,15 @@
 #Write a function called exponent(base, exp)
 # that returns an int value of base raises to the power of exp.
 
+# function
+def exponent(base, exp):
+    num = exp
+    result = 1
+    while num > 0:
+        result = result * base
+        num = num - 1
+    return result
+
 # user input
 base = int(input('enter a number:'))
 exp = int(input('enter a power:'))
